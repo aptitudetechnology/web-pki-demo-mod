fb727bf3813913b7d113f35c37fbdb689a01f567
Christopher Caston <chris@caston.id.au>
2025-06-08 13:07:57 +0800

    updated app.js for encrypt

--- js/modules/encrypt.js
+++ js/modules/encrypt.js
@@ -21,8 +21,8 @@ export class Encrypt {
  //      document.getElementById('encryptBtn').addEventListener('click', () => this.handleEncrypt());
 
         // Toggle custom public key
-        document.getElementById('toggleEncryptPublicKeyBtn').addEventListener('click', () => {
-            this.toggleCustomPublicKey();
+      //  document.getElementById('toggleEncryptPublicKeyBtn').addEventListener('click', () => {
+       //     this.toggleCustomPublicKey();
         });
     }
 
